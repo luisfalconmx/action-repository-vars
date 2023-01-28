ea17655b28f35afa9cef9347279ced73c4b16ef2
luisfalconmx <luisfalconmx@gmail.com>
2023-01-28 16:38:21 -0600

    feat: @types/node added for use process.env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
+    const vars = [];
+    console.log('ENV VARIABLES', process.env)
+    core.info('ENV VARIABLES')
+    core.info(process.env)
     core.exportVariable('LFMX', 'this is amazing!')
   } catch (error: any) {
     core.setFailed(error.message);
