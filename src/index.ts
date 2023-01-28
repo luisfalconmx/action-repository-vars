857c9f3a07f82341146e9efa8f2f5f9c9e72c29f
luisfalconmx <luisfalconmx@gmail.com>
2023-01-28 17:50:41 -0600

    feat: get repository metadata get with octokit client

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
-import * as core from '@actions/core';
+import * as core from '@actions/core'
+import { Octokit } from "@octokit/core"
+
+type Vars = {
+  name: string
+  value: string | boolean | number
+}[]
 
 async function run() {
   try {
-    const vars = [];
-    console.log('ENV VARIABLES', process.env)
-    core.info('ENV VARIABLES')
-    core.info(process.env)
-    core.exportVariable('LFMX', 'this is amazing!')
+    const GITHUB_TOKEN = <string>process.env['GITHUB_TOKEN']
+    const GITHUB_REPOSITORY = <string>process.env['GITHUB_REPOSITORY']
+    const OWNER = GITHUB_REPOSITORY.split('/')[0]
+    const REPO = GITHUB_REPOSITORY.split('/')[1]
+
+    const octokit = new Octokit({
+      auth: GITHUB_TOKEN
+    })
+    
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}', {
+      owner: OWNER,
+      repo: REPO
+    })
+
+    const vars: Vars = [
+      {
+        name: 'REPOSITORY_FULL_NAME',
+        value: data.full_name
+      }
+    ]
+
+    vars.map(variable => {
+      core.exportVariable(variable.name, variable.value)
+    })
+    
   } catch (error: any) {
     core.setFailed(error.message);
   }
